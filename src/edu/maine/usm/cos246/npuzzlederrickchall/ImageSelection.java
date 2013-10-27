9f92330e2503239efcf74a50b8e2256c6d725945
Derrick Hall <dhall@bluetarp.com>
2013-10-27 18:21:24 -0400

    #1 Initial ListView activity

--- src/edu/maine/usm/cos246/npuzzlederrickchall/ImageSelection.java
+++ src/edu/maine/usm/cos246/npuzzlederrickchall/ImageSelection.java
@@ -3,14 +3,39 @@ package edu.maine.usm.cos246.npuzzlederrickchall;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ImageSelection extends Activity {
 
+	static final String[] COLORS = new String[] {
+    	"Red",
+    	"Orange",
+    	"Yellow",
+    	"Green",
+    	"Blue",
+    	"Indigo",
+    	"Violet"
+    };
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_selection);
+        
+        ListView list = (ListView)findViewById(R.id.imageSelectionView);
+		list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, COLORS));
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		        String item = (String) parent.getItemAtPosition(position);
+				Log.i("ListMe", "You clicked:" + item);
+			}
+
+		});
     }
 
 
