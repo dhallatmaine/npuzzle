a4116296b8212b8ac545638916b0b2ae7c09e40e
Derrick Hall <dhall@bluetarp.com>
2013-10-27 22:26:43 -0400

    #3 Image displays

--- gen/edu/maine/usm/cos246/npuzzlederrickchall/R.java
+++ gen/edu/maine/usm/cos246/npuzzlederrickchall/R.java
@@ -21,18 +21,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int puzzle_0=0x7f020001;
-        public static final int puzzle_1=0x7f020002;
-        public static final int puzzle_2=0x7f020003;
+        public static final int puzzle_0=0x7f020000;
+        public static final int puzzle_1=0x7f020001;
+        public static final int puzzle_2=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int icon=0x7f080000;
-        public static final int puzzle=0x7f080001;
+        public static final int action_settings=0x7f080003;
+        public static final int icon=0x7f080001;
+        public static final int imageView1=0x7f080000;
+        public static final int puzzle=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_image_selection=0x7f030000;
+        public static final int activity_game_play=0x7f030000;
+        public static final int activity_image_selection=0x7f030001;
     }
     public static final class menu {
         public static final int image_selection=0x7f070000;
@@ -40,7 +41,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
--- src/edu/maine/usm/cos246/npuzzlederrickchall/GamePlay.java
+++ src/edu/maine/usm/cos246/npuzzlederrickchall/GamePlay.java
@@ -2,19 +2,24 @@ package edu.maine.usm.cos246.npuzzlederrickchall;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
 
 public class GamePlay extends Activity {
+	
+	ImageView image;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_play);
 		
 		Bundle bundle = this.getIntent().getExtras();
-		
 		String puzzle = bundle.getString("puzzle");
 		
-		Log.i("puzzle", puzzle);
+		int resourceId = this.getApplicationContext().getResources().getIdentifier(puzzle, "drawable", this.getApplicationContext().getApplicationInfo().packageName);
+		
+		image = (ImageView) findViewById(R.id.imageView1);
+		image.setImageResource(resourceId);
 	}
 
 }
