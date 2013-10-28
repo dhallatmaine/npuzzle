ba3211c1a34cf355515406a5e9b1a7b9d4b076db
Derrick Hall <dhall@bluetarp.com>
2013-10-28 13:03:25 -0400

    #3 Image switches after 3 seconds

--- src/edu/maine/usm/cos246/npuzzlederrickchall/GamePlay.java
+++ src/edu/maine/usm/cos246/npuzzlederrickchall/GamePlay.java
@@ -2,6 +2,7 @@ package edu.maine.usm.cos246.npuzzlederrickchall;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 public class GamePlay extends Activity {
@@ -18,8 +19,18 @@ public class GamePlay extends Activity {
 		
 		int resourceId = this.getApplicationContext().getResources().getIdentifier(puzzle, "drawable", this.getApplicationContext().getApplicationInfo().packageName);
 		
-		image = (ImageView) findViewById(R.id.imageView1);
+		final int resourceId2 = this.getApplicationContext().getResources().getIdentifier("puzzle_2", "drawable", this.getApplicationContext().getApplicationInfo().packageName);
+		
+		image = (ImageView) findViewById(R.id.solution);
 		image.setImageResource(resourceId);
+		
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				image.setImageResource(resourceId2);
+			}
+		}, 3000L);
+	    
 	}
 
 }
